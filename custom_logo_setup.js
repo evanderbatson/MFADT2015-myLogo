ace4863e078a2eab53d5772bb4fd69a4eb7c80de
Evander Batson <evanderbatson@gmail.com>
2015-05-09 18:20:45 -0400

    new

---
+++ custom_logo_setup.js
@@ -0,0 +1,15 @@
+/*------------------------------------------
+CANVAS SETUP: Please don't change
+------------------------------------------*/
+// Global variables
+var p5Canvas;       // The P5 canvas that will display the logo
+var canvasSizeInPercentage; // Canvas size relative to the width of the document
+var canvasSizeStatic; // Canvas size that is static
+var spacing;        // Distance between the center of the dots
+var diameter;       // Diameter of the dots
+var debounce;       // Wait half a second before resizing the window
+
+  spacing = 22.5;
+  diameter = 15.5;
+
+/*----------------------------------------*/
