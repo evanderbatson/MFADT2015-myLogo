18aff4db49b8a48911efe11c786badb060e79b26
gianordoli <gianordoligabriel@yahoo.com.br>
2015-05-09 19:09:37 -0400

    finished size change

---
+++ example_size_change.js
@@ -0,0 +1,61 @@
+/*------------------------------------------
+CANVAS SETUP: Please don't change
+------------------------------------------*/
+// Global variables
+var spacing;        // Distance between the center of the dots
+var diameter;       // Diameter of the dots
+var debounce;       // Wait half a second before resizing the window
+/*----------------------------------------*/
+
+function setDimensions(){
+  console.log('Called setDimensions');
+  spacing = width*0.1;
+  diameter = spacing*2/3;  
+}
+
+var allCircles = [];
+var gravity = 0.2;
+
+function setup() {
+  /*------------------------------------------
+  CANVAS SETUP: Please don't change
+  ------------------------------------------*/
+  p5Canvas = createCanvas(220, 220);
+  setDimensions();
+  /*----------------------------------------*/
+
+  // Create the circles
+  for(var x = spacing/2; x < width; x += spacing){
+    for(var y = spacing/2; y < height; y += spacing){
+      var newCircle = new Circle(x, y, diameter);
+      append(allCircles, newCircle);
+    }
+  }  
+}
+
+function draw() {
+  background(255);
+  
+  for(var i = 0; i < allCircles.length; i++){
+    allCircles[i].display();
+  }
+}
+
+function Circle(_x, _y, _diameter){
+  this.pos = {
+    x: _x,
+    y: _y    
+  }
+  this.diameter = _diameter;
+
+
+  this.display = function(){
+    this.diameter = map(dist(mouseX, mouseY, this.pos.x, this.pos.y),
+                        0, width/2,
+                        diameter, 2);
+    noFill();
+    stroke(0);
+    ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);    
+  }
+
+}
