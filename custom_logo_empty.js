b410303390f7c63aee73b46275845df58b7ac4b0
gianordoli <gianordoligabriel@yahoo.com.br>
2015-05-09 18:24:59 -0400

    no setup anymore

--- custom_logo_empty.js
+++ custom_logo_empty.js
@@ -1,3 +1,18 @@
+/*------------------------------------------
+CANVAS SETUP: Please don't change
+------------------------------------------*/
+// Global variables
+var spacing;        // Distance between the center of the dots
+var diameter;       // Diameter of the dots
+var debounce;       // Wait half a second before resizing the window
+/*----------------------------------------*/
+
+function setDimensions(){
+  console.log('Called setDimensions');
+  spacing = width*0.1;
+  diameter = spacing*2/3;  
+}
+
 function setup() {
   /*------------------------------------------
   CANVAS SETUP: Please don't change
